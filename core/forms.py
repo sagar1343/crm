7b5399c531bd0ca56f89c5f735a56199b2fa3d1f
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2026-07-15 16:53:45 +0530

    created model forms

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,21 @@ class AgentForm(forms.ModelForm):
     class Meta:
         model = Agent
         fields = "__all__"
+
+
+class PropertyForm(forms.ModelForm):
+    class Meta:
+        model = Property
+        fields = "__all__"
+
+
+class LeadForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = "__all__"
+
+
+class DealForm(forms.ModelForm):
+    class Meta:
+        model = Deal
+        fields = "__all__"
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Agent, Property, Lead, Deal
-from .forms import AgentForm
+from .forms import AgentForm, DealForm, LeadForm, PropertyForm
 
 
 # Create your views here.
@@ -14,23 +14,35 @@ def agents(request):
         form.save()
 
     queryset = Agent.objects.all()
-    context = {"agents": queryset, "form": form}
+    context = {"agents": queryset, "form": AgentForm()}
     return render(request, "pages/agents.html", context)
 
 
 def properties(request):
+    form = PropertyForm(request.POST or None, request.FILES or None)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+
     queryset = Property.objects.all()
-    context = {"properties": queryset}
+    context = {"properties": queryset, "form": PropertyForm()}
     return render(request, "pages/properties.html", context)
 
 
 def leads(request):
+    form = LeadForm(request.POST or None, request.FILES or None)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+
     queryset = Lead.objects.all()
-    context = {"leads": queryset}
+    context = {"leads": queryset, "form": LeadForm()}
     return render(request, "pages/leads.html", context)
 
 
 def deals(request):
+    form = DealForm(request.POST or None, request.FILES or None)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+
     queryset = Deal.objects.all()
-    context = {"deals": queryset}
+    context = {"deals": queryset, "form": DealForm()}
     return render(request, "pages/deals.html", context)
